415468d8fcb72b68cfcc8850db7778c704b24e80
schlos <schlos@users.noreply.github.com>
2014-09-20 20:34:26 +0200

    Update additional.js

--- app/views/additional.js
+++ app/views/additional.js
@@ -5,8 +5,8 @@ module.exports = View.extend({
   template: `
     <div class="commentView">
       <div class="comments">
-        <strong>Additional Projects</strong>
-        <p>A portion of funding will be spent strategically on various programs across the city. These programs range from ADA compliance to complete street renovations. These stand-alone programs are detailed below.</p>
+        <strong>Dodatni projekti</strong>
+        <p>Dio sredstava će biti strateški raspodjeljen na raznim programima diljem grada. Ti samostalni programi su opisani u nastavku.</p>
         <div role="additionalProjects"></div>
       </div>
     </div>
