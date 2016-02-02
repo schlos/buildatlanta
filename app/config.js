182af37b156fe8b8a82d3d2dd0e539314d947b6f
Miroslav Schlossberg <mschlos@gmail.com>
2016-02-02 23:10:07 +0100

    add zag data

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   map: {
-    center: [33.748481, -84.388375],
+    center: [45.8403496, 15.8242475],
     zoomLevel: 12
   },
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiY29kZWZvcmFtZXJpY2EiLCJhIjoiSTZlTTZTcyJ9.3aSlHLNzvsTwK-CYfZsG_Q',
-    mapId: 'codeforamerica.j379fok7'
+    accessToken: 'pk.eyJ1Ijoic2NobG9zIiwiYSI6IklKQjFhRDQifQ.7X1cC-OmIOBgWBt9Djxe4Q',
+    mapId: 'schlos.oll5foon'
   }
 };
